28ed814c106b05012d36cd2acbbf534bdddfc2bd
Jeewon Koo <47291707+jeewonkoo@users.noreply.github.com>
2022-09-27 22:58:14 -0500

    Time: 114 ms (11.63%), Space: 14.6 MB (82.52%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.py
@@ -0,0 +1,6 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target in nums: return nums.index(target)
+        for i,v in enumerate(nums):
+            if v > target: return i
+        return len(nums)
