52c767a632fe6bd39f282ba3be552c8f51ed2615
Jeewon Koo <47291707+jeewonkoo@users.noreply.github.com>
2022-11-09 00:16:57 -0600

    Time: 86 ms (37.79%), Space: 13.8 MB (71.36%) - LeetHub

---
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.py
@@ -0,0 +1,9 @@
+class Solution:
+    def greatestLetter(self, s: str) -> str:
+        res = ""
+        for i in s:
+            if i.isupper() and i.lower() in s:
+                if i > res:
+                    res = i.upper()
+                        
+        return res
