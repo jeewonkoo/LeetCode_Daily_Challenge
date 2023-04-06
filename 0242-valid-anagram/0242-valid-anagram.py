f0527667b36c2cbb3aa0e9af2b76ddcdab047124
Jeewon Koo <47291707+jeewonkoo@users.noreply.github.com>
2023-04-06 00:03:27 -0500

    Time: 48 ms (72.73%), Space: 14.4 MB (59.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return collections.Counter(s) == collections.Counter(t)
+        def createDictionary(string):
+            dic = {}
+            for i in string:
+                dic[i] = dic.get(i, 0) + 1
+            return dic 
+        
+        s_dic = createDictionary(s)
+        t_dic = createDictionary(t)
+        
+        return s_dic == t_dic
