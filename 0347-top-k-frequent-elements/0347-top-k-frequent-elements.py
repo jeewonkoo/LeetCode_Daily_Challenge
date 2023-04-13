350dd1d5be471b163f0abc2c58c3aea8cb8c6553
Jeewon Koo <47291707+jeewonkoo@users.noreply.github.com>
2023-04-13 03:34:08 -0500

    Time: 100 ms (83.62%), Space: 18.5 MB (99.10%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,7 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        d = {}
+        for i in nums:
+            d[i] = d.get(i, 0) + 1
+        
+        return heapq.nlargest(k, d.keys(), key=d.get)
